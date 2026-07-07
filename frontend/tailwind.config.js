0848b12fe7c207969697a184bc0ec3bd0ff60142
Aniket Kshirsagar <kshirsagarask19@gmail.com>
2026-07-07 06:35:57 -0400

    chore: scaffold APEX XI frontend (Vite+React+TS+Tailwind+Vitest)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: { extend: { colors: { navy: '#0b1f3a', pitch: '#1f7a4d', gold: '#c9a227' } } },
+  plugins: [],
+}
